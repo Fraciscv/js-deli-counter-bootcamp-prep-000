d8bcb26af788437aabdc2c1db8ab7314ea978291
Francisco Andaur <francisco.andaur@icloud.com>
2020-02-28 17:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+
+function nowServing(KatzDeliLine) {
+  if (KatzDeliLine.length === 0)
+    return 'There is nobody waiting to be served!';
+} else {
+    return `Currently serving ${KatzDeliLine.shift()}`;
+}
